891766b3ee7981717065bc8ccc2033008e2a3339
Ramzes_13 <rslobolinsky13@gmail.com>
2024-04-22 15:15:29 +0400

    fixies

--- vlog/admin.py
+++ vlog/admin.py
@@ -4,6 +4,6 @@ from vlog.models import Vlog
 
 
 @admin.register(Vlog)
-class ArticleAdmin(admin.ModelAdmin):
+class VlogAdmin(admin.ModelAdmin):
     list_display = ['pk', 'name', 'text']
     search_fields = ['name', 'text']
