8876669870e90192d691477d180c1b8b661f8ea2
Ramzes_13 <rslobolinsky13@gmail.com>
2024-05-28 17:58:12 +0400

    add homework CACHE

--- catalog/urls.py
+++ catalog/urls.py
@@ -9,7 +9,7 @@ from catalog.views import ProductListView, ContactsPageView, ProductDetailView, 
 app_name = CatalogConfig.name
 urlpatterns = [
     path('', ProductListView.as_view(), name='home'),
-    path('product/<int:pk>/', cache_page(60), (ProductDetailView.as_view()), name='product'),
+    path('product/<int:pk>', cache_page(60)(ProductDetailView.as_view()), name='product'),
     path('contacts/', ContactsPageView.as_view(), name='contacts'),
     path('product/create', ProductCreateView.as_view(), name='create'),
     path('product/update/<int:pk>', ProductUpdateView.as_view(), name='update'),
